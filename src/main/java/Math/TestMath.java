dfcd1d181c1336ba1e4c37500d5e7e26d782085b
olcaytaner <olcaytaner@gmail.com>
2019-03-14 22:15:59 +0300

    no message

--- src/main/java/Math/TestMath.java
+++ src/main/java/Math/TestMath.java
@@ -5,27 +5,10 @@ import java.util.ArrayList;
 public class TestMath {
 
     public static void main(String[] args){
-        Matrix m = new Matrix(4, 4);
-        m.setValue(0, 0, 2);
-        m.setValue(0, 1, 0);
-        m.setValue(0, 2, -2);
-        m.setValue(0, 3, 0);
-        m.setValue(1, 0, 0);
-        m.setValue(1, 1, 2);
-        m.setValue(1, 2, 0);
-        m.setValue(1, 3, -2);
-        m.setValue(2, 0, -2);
-        m.setValue(2, 1, 0);
-        m.setValue(2, 2, 2);
-        m.setValue(2, 3, 0);
-        m.setValue(3, 0, 0);
-        m.setValue(3, 1, -2);
-        m.setValue(3, 2, 0);
-        m.setValue(3, 3, 2);
+        Matrix m = new Matrix(1000, 1000, 0.0, 1.0);
         try {
-            ArrayList<Eigenvector> result = m.characteristics();
-        } catch (MatrixNotSymmetric matrixNotSymmetric) {
-        } catch (MatrixNotSquare matrixNotSquare) {
+            m.inverse();
+        } catch (DeterminantZero | MatrixNotSquare error) {
         }
     }
 }
